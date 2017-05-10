cf29d24d2543f7dbfdf8fc1034f588beda02f6cb
Sebastien Duthil <sduthil@wazo.community>
2017-05-10 10:57:47 -0400

    add hamcrest matcher for UUID str

---
+++ xivo_test_helpers/hamcrest/uuid.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from functools import partial
+from hamcrest import matches_regexp
+
+
+uuid = partial(matches_regexp, '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
