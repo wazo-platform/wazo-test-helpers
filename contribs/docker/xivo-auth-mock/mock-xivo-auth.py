wazo-platform/wazo-test-helpers

commit dcf5cdf4ebde48c42f9013cff55f92c126115ec2

auth-mock: allow setting other token attributes
e.g. xivo_uuid

contribs/docker/xivo-auth-mock/mock-xivo-auth.py
@@ -13,7 +13,7 @@ port = int(sys.argv[1])
 
 context = ('/usr/local/share/ssl/auth/server.crt', '/usr/local/share/ssl/auth/server.key')
 
-valid_tokens = {'valid-token': 'uuid'}
+valid_tokens = {'valid-token': {'auth_id': 'uuid'}}
 wrong_acl_tokens = {'invalid-acl-token'}
 invalid_username_passwords = [('test', 'foobar')]
 token_that_will_be_invalid_when_used = [('test', 'iddqd')]
@@ -23,9 +23,8 @@ token_that_will_be_invalid_when_used = [('test', 'iddqd')]
 def add_token():
     request_body = request.get_json()
     token = request_body['token']
-    auth_id = request_body['auth_id']
 
-    valid_tokens[token] = auth_id
+    valid_tokens[token] = request_body
 
     return '', 204
 
@@ -45,12 +44,10 @@ def token_get(token):
     if token not in valid_tokens:
         return '', 404
 
+    result = dict(valid_tokens[token])
+    result.setdefault('xivo_user_uuid', result['auth_id'])
     return jsonify({
-        'data': {
-            'auth_id': valid_tokens[token],
-            'token': token,
-            'xivo_user_uuid': valid_tokens[token]
-        }
+        'data': result
     })
 
 

xivo_test_helpers/auth.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import requests
-
 import logging
+import requests
+import uuid
+
 logger = logging.getLogger(__name__)
 
 
@@ -34,12 +35,14 @@ class AuthClient(object):
 
 class MockUserToken(object):
 
-    def __init__(self, token, user_uuid):
+    def __init__(self, token, user_uuid, wazo_uuid=None):
         self._token = token
         self._auth_id = user_uuid
+        self._wazo_uuid = wazo_uuid or str(uuid.uuid4())
 
     def to_dict(self):
         return {
             'token': self._token,
             'auth_id': self._auth_id,
+            'xivo_uuid': self._wazo_uuid,
         }

commit bcc2c3ab0abc166a4e64ccd64f4fcca8a56fc776

fix mock-xivo-auth.py
bug introduced by commit:
dcf5cdf4ebde48c42f9013cff55f92c126115ec2

contribs/docker/xivo-auth-mock/mock-xivo-auth.py
@@ -13,7 +13,7 @@ port = int(sys.argv[1])
 
 context = ('/usr/local/share/ssl/auth/server.crt', '/usr/local/share/ssl/auth/server.key')
 
-valid_tokens = {'valid-token': {'auth_id': 'uuid'}}
+valid_tokens = {'valid-token': {'auth_id': 'uuid', 'token': 'valid-token'}}
 wrong_acl_tokens = {'invalid-acl-token'}
 invalid_username_passwords = [('test', 'foobar')]
 token_that_will_be_invalid_when_used = [('test', 'iddqd')]
@@ -57,10 +57,10 @@ def token_post():
     if auth in invalid_username_passwords:
         return '', 401
     elif auth in token_that_will_be_invalid_when_used:
-        return jsonify({'data': {'auth_id': valid_tokens['valid-token'],
+        return jsonify({'data': {'auth_id': valid_tokens['valid-token']['auth_id'],
                                  'token': 'expired'}})
     else:
-        return jsonify({'data': {'auth_id': valid_tokens['valid-token'],
+        return jsonify({'data': {'auth_id': valid_tokens['valid-token']['auth_id'],
                                  'token': 'valid-token'}})
 
 