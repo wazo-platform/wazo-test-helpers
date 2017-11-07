536784129faf7149a95e855e07e0a2c5259dce2f
Francois Blackburn <fblackburn@wazo.community>
2017-11-07 10:07:44 -0500

    confd: fix missing copyright/license/utf8

--- xivo_test_helpers/confd/helpers/voicemail.py
+++ xivo_test_helpers/confd/helpers/voicemail.py
@@ -1,3 +1,7 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
 from . import confd
 from .. import config
 from .context import generate_context
