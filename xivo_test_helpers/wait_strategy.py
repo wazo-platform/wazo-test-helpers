0ca1d75dcbb243973725ce2e9409756a0a566440
Sebastien Duthil <sduthil@wazo.community>
2018-10-17 13:18:04 -0400

    add wait_strategy.py
    Those classes are used in integration tests, to wait for services to be
    ready for tests

---
+++ xivo_test_helpers/wait_strategy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+
+class WaitStrategy(object):
+
+    def wait(self, integration_test):
+        raise NotImplementedError()
+
+
+class NoWaitStrategy(WaitStrategy):
+
+    def wait(self, integration_test):
+        pass
