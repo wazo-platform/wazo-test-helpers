4507fd531bf950492e16698c1d8ba9121766a84c
Francois Blackburn <fblackburn@wazo.community>
2018-11-10 11:32:29 -0500

    add has_callable helpers

--- xivo_test_helpers/hamcrest/has_callable.py
+++ xivo_test_helpers/hamcrest/has_callable.py
@@ -17,25 +17,25 @@ __copyright__ = "Copyright 2011 hamcrest.org"
 __license__ = "BSD, see License.txt"
 
 
-class IsObjectWithProperty(BaseMatcher):
+class IsObjectWithCallable(BaseMatcher):
 
-    def __init__(self, property_name, value_matcher):
-        self.property_name = property_name
+    def __init__(self, callable_name, value_matcher):
+        self.callable_name = callable_name
         self.value_matcher = value_matcher
 
     def _matches(self, o):
         if o is None:
             return False
 
-        if not hasattr(o, self.property_name):
+        if not hasattr(o, self.callable_name):
             return False
 
-        value = getattr(o, self.property_name)
+        value = getattr(o, self.callable_name)()
         return self.value_matcher.matches(value)
 
     def describe_to(self, description):
-        description.append_text("an object with a property '") \
-                                        .append_text(self.property_name) \
+        description.append_text("an object with a callable '") \
+                                        .append_text(self.callable_name) \
                                         .append_text("' matching ") \
                                         .append_description_of(self.value_matcher)
 
@@ -44,17 +44,17 @@ class IsObjectWithProperty(BaseMatcher):
             mismatch_description.append_text('was None')
             return
 
-        if not hasattr(item, self.property_name):
+        if not hasattr(item, self.callable_name):
             mismatch_description.append_description_of(item) \
                                 .append_text(' did not have the ') \
-                                .append_description_of(self.property_name) \
-                                .append_text(' property')
+                                .append_description_of(self.callable_name) \
+                                .append_text(' callable')
             return
 
-        mismatch_description.append_text('property ') \
-                            .append_description_of(self.property_name) \
+        mismatch_description.append_text('callable ') \
+                            .append_description_of(self.callable_name) \
                             .append_text(' ')
-        value = getattr(item, self.property_name)
+        value = getattr(item, self.callable_name)()
         self.value_matcher.describe_mismatch(value, mismatch_description)
 
     def __str__(self):
@@ -63,27 +63,27 @@ class IsObjectWithProperty(BaseMatcher):
         return str(d)
 
 
-def has_property(name, match=None):
-    """Matches if object has a property with a given name whose value satisfies
+def has_callable(name, match=None):
+    """Matches if object has a callable with a given name whose value satisfies
     a given matcher.
-    :param name: The name of the property.
+    :param name: The name of the callable.
     :param match: Optional matcher to satisfy.
-    This matcher determines if the evaluated object has a property with a given
-    name. If no such property is found, ``has_property`` is not satisfied.
-    If the property is found, its value is passed to a given matcher for
+    This matcher determines if the evaluated object has a callable with a given
+    name. If no such callable is found, ``has_callable`` is not satisfied.
+    If the callable is found, its value is passed to a given matcher for
     evaluation. If the ``match`` argument is not a matcher, it is implicitly
     wrapped in an :py:func:`~hamcrest.core.core.isequal.equal_to` matcher to
     check for equality.
     If the ``match`` argument is not provided, the
     :py:func:`~hamcrest.core.core.isanything.anything` matcher is used so that
-    ``has_property`` is satisfied if a matching property is found.
+    ``has_callable`` is satisfied if a matching callable is found.
     Examples::
-        has_property('name', starts_with('J'))
-        has_property('name', 'Jon')
-        has_property('name')
+        has_callable('__str__', starts_with('J'))
+        has_callable('__str__', 'Jon')
+        has_callable('__str__')
     """
 
     if match is None:
         match = anything()
 
-    return IsObjectWithProperty(name, wrap_shortcut(match))
+    return IsObjectWithCallable(name, wrap_shortcut(match))
