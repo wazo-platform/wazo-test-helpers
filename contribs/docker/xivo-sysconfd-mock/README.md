9540b110ed3fc69b62912fdccfcc40707c2ff337
Sebastien Duthil <sduthil@wazo.community>
2018-11-22 14:17:00 -0500

    add xivo-sysconfd-mock

---
+++ contribs/docker/xivo-sysconfd-mock/README.md
@@ -0,0 +1,9 @@
+# xivo-sysconfd mock
+
+This is the required files to build a docker image to be used as a xivo-sysconfd mock
+for integration tests.
+
+Usage
+---
+
+This mock exposes an empty interface made for asserting which routes are called.
---
+++ contribs/docker/xivo-sysconfd-mock/xivo-sysconfd-mock.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python2
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import sys
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+port = sys.argv[1]
+
+REQUESTS = []
+
+
+@app.before_request
+def log_request():
+    if not request.path.startswith('/_requests'):
+        path = request.path
+        log = {'method': request.method,
+               'path': path,
+               'query': dict(request.args.items()),
+               'body': request.data,
+               'headers': dict(request.headers)}
+        REQUESTS.append(log)
+
+
+@app.route('/_requests', methods=['GET'])
+def list_requests():
+    return jsonify(requests=REQUESTS)
+
+
+@app.route('/_requests', methods=['DELETE'])
+def delete_requests():
+    global REQUESTS
+    REQUESTS = []
+    return ''
+
+
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def fallback(path):
+    return ''
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
