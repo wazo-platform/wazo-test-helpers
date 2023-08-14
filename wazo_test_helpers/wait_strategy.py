ad3e80c3732a959f7d9b46052ed8cdddbedfa74f
Francois Blackburn <fblackburn@wazo.community>
2023-08-14 11:12:53 -0400

    add generic ComponentsWaitStrategy helper
    note: typing has been added without mypy, to avoid to clash with in
    progress branch (separate-test-asset-helper)

--- wazo_test_helpers/wait_strategy.py
+++ wazo_test_helpers/wait_strategy.py
@@ -1,8 +1,14 @@
 # Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
 from __future__ import annotations
 
+import requests
+
+from abc import ABCMeta, abstractmethod
 from collections.abc import Callable
+
+from . import until
 
 
 class WaitStrategy:
@@ -13,3 +19,24 @@ class WaitStrategy:
 class NoWaitStrategy(WaitStrategy):
     def wait(self, integration_test: Callable[..., None]) -> None:
         pass
+
+
+class ComponentsWaitStrategy(WaitStrategy, metaclass=ABCMeta):
+    @abstractmethod
+    def get_status(self, integration_test: Callable[..., None]) -> dict:
+        pass
+
+    def __init__(self, components: list[str]):
+        self._components = components
+
+    def wait(self, integration_test: Callable[..., None]) -> None:
+        def components_are_ok(components) -> None:
+            try:
+                status = self.get_status(integration_test)
+            except requests.RequestException:
+                status = {}
+
+            for component in components:
+                assert status[component]['status'] == 'ok'
+
+        until.assert_(components_are_ok, self._components, timeout=10)
